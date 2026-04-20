3825058c4c6d9144e563b1088827d5b87e416847
Yusuf Sunnatullayev <yusuffdeveloper@gmail.com>
2026-04-20 21:45:23 +0500

    type annotations completed

--- fetchjson/annotations/variables.ts
+++ fetchjson/annotations/variables.ts
@@ -27,3 +27,33 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When to use annotations 🚩
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
+
+// 2) When we declare a variable on one line and initialize it later
+let words = ["red", "green", "blue"];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === "green") {
+    foundWord = true;
+  } else {
+    foundWord = false;
+  }
+}
+
+// 3) When we want to declare a variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  } else {
+    numberAboveZero = false;
+  }
+}
