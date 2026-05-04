d1e8bf25697d49e4383a97365373c802bafa01cb
Yusuf <s.yusuf@uniconsoft.uz>
2026-05-04 10:45:14 +0500

    stats

---
+++ stats/build/index.js
@@ -0,0 +1,32 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const matches = fs_1.default
+    .readFileSync("football.csv", {
+    encoding: "utf-8",
+})
+    .split("\n")
+    .map((row) => {
+    return row.split(",");
+});
+// enum - enumeration
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+let manUnitedWins = 0;
+for (let match of matches) {
+    if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games`);
+//# sourceMappingURL=index.js.map
