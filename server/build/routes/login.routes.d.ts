893ebb502fd994d8462fdd0ff476fca66be5d86c
Yusuf <s.yusuf@uniconsoft.uz>
2026-05-18 12:13:40 +0500

    server

---
+++ server/build/routes/login.routes.d.ts
@@ -0,0 +1,3 @@
+declare const router: import("express-serve-static-core").Router;
+export { router };
+//# sourceMappingURL=login.routes.d.ts.map
---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { router } from "./routes/login.routes";
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
+
+app.listen(3000, () => {
+  console.log("Listening on port: 3000");
+});
